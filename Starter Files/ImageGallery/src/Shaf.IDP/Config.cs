5fb9d86f6a6408e7dee029b2c566eb696cb5dfdf
Shaf <sharfarazhassan@gmail.com>
2018-11-07 20:31:26 -0500

    Initial commit

---
+++ Starter Files/ImageGallery/src/Shaf.IDP/Config.cs
@@ -0,0 +1,77 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Shaf.IDP
+{
+    public static class Config
+    {
+        //holds IN-MEMORY configuration 
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    //the subjectId is  their unique identifier 
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "Frank",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        //a "claim" represents information about a user 
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood"),
+                        new Claim("address", "Main Road 1"),
+                        new Claim("role", "FreeUser"),
+                        new Claim("subscriptionlevel", "FreeUser"),
+                        new Claim("country", "nl")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Claire",
+                    Password = "password",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood"),
+                        //new Claim("address", "Big Street 2"),
+                        //new Claim("role", "PayingUser"),
+                        //new Claim("subscriptionlevel", "PayingUser"),
+                        //new Claim("country", "be")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                //THESE ARE SCOPES
+
+                //Identity Resources map to scopes that give identity-related information 
+                //since we're using Open ID connect, we must support the Open ID scope 
+                new IdentityResources.OpenId(),
+                //OpenId() ensures that the SubjectId is included 
+                //if the client requests the OpenId() scope, the the user identiifer claim (which is SubjectId) is  returned
+                new IdentityResources.Profile(),
+                //the Profile scope maps to profile-related claims like the ones in the list of claims
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>(); 
+        }
+    }
+}
---
+++ Starter Files/ImageGallery/src/Shaf.IDP/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shaf.IDP
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(); 
+            //register the Identity Server services on ASP.Net Core's built-in dependency container 
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddTestUsers(Config.GetUsers())
+                //IN-MEMORY methods for now - this will be different 
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryClients(Config.GetClients());
+                //sets up a credential to sign the tokens with - should be changed to a REAL certificate in Production
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //add Identity Server to the request pipeline
+            app.UseIdentityServer();
+            app.UseStaticFiles();
+            //UseStaticFiles() method is called because it's able to mark files in web root as servable. 
+            //For example: href="~/favicon.ico" The tilde "~" sign points to webroot (wwwroot) so the real URL is: wwwroot/favicon.ico
+            //Without this method, the image path would not be valid and nothing would show. 
+            app.UseMvcWithDefaultRoute(); 
+        }
+    }
+}
